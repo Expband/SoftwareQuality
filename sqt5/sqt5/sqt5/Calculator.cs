b79d101c44517720703b41b7bd8c95209408efe9
Expband <80225999+Expband@users.noreply.github.com>
2023-03-26 23:36:21 +0200

    Sqt lab 5 with report and screenshots , also code in C#

---
+++ sqt5/sqt5/sqt5/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace sqt5
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+        }
+        public void Validation()
+        {
+            Console.Write("a = ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            if ((a - (2 * b)) !=0)
+            {
+                double[] values = new double[2];
+                values[0] = a;
+                values[1] = b;
+                Calculate(values);
+            }
+            else Console.WriteLine("this values a wrong");
+        }
+        protected double Calculate(double[] values)
+        {
+            double Result = (((2 * values[0]) - values[1]) * ((2 * values[1]) + values[0])) / (values[0] - (2 * values[1]));
+            DisplayDaya(Result);
+            return Result;
+            
+        }
+        public void DisplayDaya(double Result)
+        {
+            Console.WriteLine(Result);
+        }
+    }
+}
---
+++ sqt5/sqt5/sqt5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace sqt5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            calculator.Validation();
+            
+        }
+    }
+}
