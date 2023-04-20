f06794560cdf1ddc2a73cb9d9fdda0785669b430
Expband <80225999+Expband@users.noreply.github.com>
2023-04-20 20:33:38 +0200

    SQT10 lab with source code and report

---
+++ sqt10/sqt10/sqt10/Program.cs
@@ -0,0 +1,51 @@
+﻿//using System;
+
+//class Program
+//{
+//    static void Sorter()
+//    {
+//        int[] array = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+//        int[] array1 = new int[array.Length];
+
+//        int index = 0;
+//        for (int i = 0; i < array.Length; i += 2)
+//        {
+//            try
+//            {
+//                array1[index] = array[i];
+//                index++;
+//            }
+//            catch (Exception)
+//            {
+//                // Do nothing.
+//            }
+//        }
+
+//        for (int i = 1; i < array.Length; i += 2)
+//        {
+//            try
+//            {
+//                array1[index] = array[i];
+//                index++;
+//            }
+//            catch (Exception)
+//            {
+//                // Do nothing.
+//            }
+//        }
+
+//        Array.Resize(ref array1, index);
+//        Console.WriteLine(string.Join(", ", array1));
+//    }
+//    static void Main(string[] args)
+//    {
+//        Sorter();
+//    }
+//}
+Console.WriteLine(" ");
+
+
+
+
+
+
